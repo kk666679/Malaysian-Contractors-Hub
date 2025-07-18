a7ee2584a502eacad9c858adf5903b08bc3a80bb
Admin <admin@matmotofix.pro>
2025-07-18 22:18:10 +0000

    Add project configuration files

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import reactSWC from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [reactSWC()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
